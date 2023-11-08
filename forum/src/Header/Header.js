c94fd1245e86d78380c4e7e6c78df09dd05f76fb
Paulo <124280407+PDSentinella@users.noreply.github.com>
2023-11-08 14:01:31 +0000

    tile wind setup and hearder-v1

---
+++ forum/src/Header/Header.js
@@ -0,0 +1,23 @@
+
+
+
+function Header() {
+    return (
+    <nav className="w-screen">
+      <div className="w-screen flex justify-between bg-red-600 h-24 items-center">
+        <div className="flex">
+          <h1>FORUM IPT LOGO</h1>
+        </div> 
+        
+        <div className="flex items-center justify-between w-24 p-1 border-2 border-black">
+          <div className="flex-none  rounded-full bg-gray-200 w-6 h-6"></div>
+          
+        </div>
+      </div>
+    </nav> 
+      
+    );
+  }
+  
+  export default Header;
+  
