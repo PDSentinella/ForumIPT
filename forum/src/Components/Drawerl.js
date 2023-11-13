cc8136c43064668a928962640dc1b6e55684663d
Paulo <124280407+PDSentinella@users.noreply.github.com>
2023-11-13 23:23:56 +0000

    Update Drawerl.js

---
+++ forum/src/Components/Drawerl.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Drawerl(){
+    return (
+      <div className='flex justify-between mx-4 py-4 items-center'>
+        <div>side bar</div>
+      </div>
+    )
+  }
+
+export default Drawerl
