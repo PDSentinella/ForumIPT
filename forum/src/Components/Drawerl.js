96b65b17f08832f9e2ef8052f5697014bdf9fce0
Paulo <124280407+PDSentinella@users.noreply.github.com>
2023-11-14 11:45:24 +0000

    ...

--- forum/src/Components/Drawerl.js
+++ forum/src/Components/Drawerl.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import Icon from './SubForum'
+const channels = [
+  {
+  notification:2,
+  name:"redes",
+  },
+  {
+    notification:0,
+    name:"Interface Web",
+  },
+  {
+    notification:3,
+    name:"base De Dados"
+  }
+
+]
+
 function Drawerl(){
     return (
-      <div className='flex justify-between  h-full w-40 p-10 bg-cadet-gray'>
-        <div>side bar</div>
-        
+      <div className=' flex flex-col h-full w-40 p-10 bg-cadet-gray'>
+        <div className='flex'>side bar</div>
+        <ul>
+        {channels.map( (element) => (
+           <incons>{element.name}</incons>          
+        ))}
+        </ul>
       </div>
     )
   }
--- forum/src/Components/SubForum.js
+++ forum/src/Components/SubForum.js
@@ -1,14 +1,11 @@
-
+import {useState} from 'react'
 import React from 'react';
-const props = {
-    notification:2,
-    name:Redes
-}
-function Icon(){
+
+function Icon(props){
     return <div className="flex">
         <div><img /*src={props.img} *//></div>
         <div><h1>props.name</h1></div>
-        <div className={'${props.notifications==0 && hidden}'}><h1>/*{props.notifications}</h1></div>
+        <div className={`${props.notification==0 && 'hidden'}`}><h1>{props.notifications}</h1></div>
     </div>
 }
 export default Icon
