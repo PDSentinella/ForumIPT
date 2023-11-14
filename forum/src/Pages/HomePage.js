dfe77ff3d2828beaaf773af9bdbf4c73eb38fc6c
Cyb3rWolf945 <59710384+Cyb3rWolf945@users.noreply.github.com>
2023-11-14 17:14:52 +0000

    Revert "footer"
    This reverts commit 6c533584a76d8dcd96879949e52878c12e51abd7.

--- forum/src/Pages/HomePage.js
+++ forum/src/Pages/HomePage.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Header from '../Components/Header'
-import Footer from '../Components/Footer'
 
 function HomePage() {
   return (
     <>
     <Header></Header>
     <div className='bg-alice-blue h-full w-full'>HomePage</div>
-    <Footer></Footer>
     </>
   )
 }
