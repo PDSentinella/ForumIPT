e7877e310573c520b798041f08696e169aed5458
Paulo <124280407+PDSentinella@users.noreply.github.com>
2023-11-21 22:49:36 +0000

    .

--- forum/src/Components/UserInfo.js
+++ forum/src/Components/UserInfo.js
@@ -11,7 +11,7 @@ const UserInfo = () => {
   return (
     <div className='flex flex-col justify-between p-4 items-center w-full h-full gap-y-8 pt-16'>
         <h3>aluno24880@ipt.pt</h3>
-        <div className='flex flex-wrap items-center  justify-center gap-y-8  py-8 pb-18'>
+        <div className='flex flex-wrap items-center lg:flex-co justify-center gap-y-8  py-8 pb-18'>
             <div className='flex flex-col w-56 items-center'>
                 <h3 className='font-light'>Telephone</h3>
                 <h3 className='font-bold'>+351 960360141</h3>
@@ -29,8 +29,8 @@ const UserInfo = () => {
                 <h3 className='font-bold'>Student</h3>
             </div>            
         </div>
-
-            <div className='flex flex-col w-full pb-8 justify-center '>
+            <div className='flex flex-col items-center justify-center'>
+            <div className='flex flex-col w-full pb-8 items-start justify-center '>
                     <h3 className='text-2x '>About Me</h3>
                     <p className='font-light'>As my days went by, i found that loving and carraing are important, but there are also other important think in the world. Hello my name is Paulo i am a second year student of CS, and i am looking for a job. </p>
             </div>
@@ -42,6 +42,7 @@ const UserInfo = () => {
                         <div className=' text-xs text-alice-blue px-2 py-1 rounded-full bg-ipt'>{canal}</div>
                     ))}
                 </div>
+            </div>
             </div>
     </div>
   )
