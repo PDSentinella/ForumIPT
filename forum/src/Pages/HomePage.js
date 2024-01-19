e9a472541b63fe9285fe2071276079f88c165108
Paulo <124280407+PDSentinella@users.noreply.github.com>
2024-01-19 18:36:58 +0000

    .

--- forum/src/Pages/HomePage.js
+++ forum/src/Pages/HomePage.js
@@ -5,66 +5,44 @@ import SideBar from '../Components/SideBar'
 import Post from '../Components/Post'
 import AddPost from '../Components/addPost'
 import Bar from '../Components/Bar'
+//import getPublicacao from '../utils/getPublicacao'
+import getUsers from '../utils/getsUser'
 
+async function getPublicacao(filter="") {
+  let url = 'https://api.sheety.co/fb92c09eaad214da086d51fb7c8f7735/ipt/publicacao'+"/"+filter;
+  let value;
+  await fetch(url)
+  .then((response) => response.json())
+  .then(json => {
+  // Do something with the data
+   value = json.publicacao
+  //console.log(value)
+  
+  });
+  
+  return value;
+  
+  
+}
 
-function getPublicatoin(publicationcount){
-  let publicacao = [{
+function getPublication(publicationcount){
+  let publicacao = {
     titulo:"Date of the final exams",
     img:"",
     user:{name:"Paulo Santo",
         foto:"https://w0.peakpx.com/wallpaper/367/169/HD-wallpaper-heart-aesthetic.jpg"       
         },
-    time:"2 hrs ago",
     msg:"Dear Students\n I want to inform you that after 6 moths of our cooperation it is necessary to test you knowlege by th final exam, It means we need to find a date for our final exam, In this semester you were extremely under the stress due to",
-    coments:[
-    {
-        user:{
-                name:"Paulo",
-                foto:"https://w0.peakpx.com/wallpaper/367/169/HD-wallpaper-heart-aesthetic.jpg",
-                teacher:true
-            },
-        commentMsg:"Good after noon teacher. I think that 18 Dezember will be a good date to do the test",
-        time:"2hrs ago",
-        teacher:false
-    }  ,
-
-    {
-        user:{
-                name:"Toninho",
-                foto:"",
-                teacher:false
-            },
-        commentMsg:"Good afternoon. I think that 18 dezember will ber an excellent day to do our asigniment",
-        time:"12/1/4",
-    },
-
-            ]
-}]
+    
+} 
+console.log(publicacao)
   return publicacao
 }
 function HomePage() {
   const [publicationcount, setPublicationcount] = useState(5);
-  const [publicatoins, setPublications] = useState(getPublicatoin(publicationcount));
+  const [publications, setPublications] = useState(getPublication(publicationcount));
 
   useEffect(() => {
-    // Update the document title using the browser API
-    // localStorage.setItem("login", {islogin});
-    /*<div className="flex  h-screen overflow-hidden ">
-
-        <SideBar></SideBar>
-        <div className='bg-background-gray  sm:px-6  px-2 flex flex-1 h-full font-semibold w-full overflow-y-auto justify-center'>
-          <div className='flex flex-col w-full '>
-          <Header></Header>
-          <div className='flex flex-col items-center'>
-          <AddPost></AddPost>
-          <Post></Post>
-          <Post></Post>
-          <Post></Post>
-          </div>
-          <Footer></Footer>
-          </div>
-        </div>
-      </div>*/
   });
  
   ///lista de publicações
@@ -82,9 +60,8 @@ function HomePage() {
           <Header></Header>
           <div className='flex flex-col items-center '>
           <AddPost></AddPost>
-          {publicatoins.map((publication) => (
-             <Post publicacao={publication}></Post>
-        ))}
+          
+             <Post publicacao={publications}></Post>
          
          
           </div>
