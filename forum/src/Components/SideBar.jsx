65742812b5c988fee77b0f429d3c3ab3cc8af9c3
Cyb3rWolf945 <59710384+Cyb3rWolf945@users.noreply.github.com>
2024-01-24 14:30:22 +0000

    update icons #1
    update icons #1

--- forum/src/Components/SideBar.jsx
+++ forum/src/Components/SideBar.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import React, { useState } from 'react'
 import Drawerl from './Drawerl';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import BookmarkAddedOutlinedIcon from '@mui/icons-material/BookmarkAddedOutlined';
+import MessageOutlinedIcon from '@mui/icons-material/MessageOutlined';
 //instagram side bar, quando a screen é grande o suficiente a side bar expande de vez, podemos recriar algo parecido usando xs-max: ou algo do genero, tentar
 function SideBar(){
     const [open, setOpen] = useState(false);
@@ -18,34 +21,27 @@ function SideBar(){
                 </div>
 
                 <div className={`flex flex-col flex-1 justify-center gap-y-6 pt-8 ${!open ?'items-center w-full ':'items-start ml-14 w-62'}`}>
-                <Link to={"/"} reloadDocument><div className={`flex gap-x-2 justify-center cursor-pointer `}>
-                        <div className={` flex rounded items-center justify-center bg-white w-6 h-6 `}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" PencilIcon color='#88b77b' viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 place-self-center">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                            </svg>
+                <Link to="/" reloadDocument><div className={`flex gap-x-2 justify-center cursor-pointer `}>
+                        <div className={` flex rounded items-center justify-center w-6 h-6 `}>
+                          <HomeOutlinedIcon sx={{ color: 'white', fontSize: 32}}/>  
                         </div>
 
                         <h1 className={`${!open&&'hidden'}  text-white font-bold tracking-wide`}>Home</h1>
                     </div>
-                    </Link> 
+                </Link> 
                     {/*Home*/}
                     
                     <div className='flex gap-x-2 items-center cursor-pointer'>
-                        <div className=' flex rounded items-center justify-center bg-white w-6 h-6 '>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" PencilIcon color='#88b77b' viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
-                        </svg>
-
+                        <div className=' flex rounded items-center justify-center w-6 h-6 '>
+                        <MessageOutlinedIcon sx={{ color: 'white', fontSize: 32}}/>  
                         </div>
 
-                        <h1 className={`${!open&&'hidden'}  text-white font-bold tracking-wide`}>Your treads</h1>
+                        <h1 className={`${!open&&'hidden'}  text-white font-bold tracking-wide`}>Mensagens</h1>
                     </div>                     
                     {/*Your treads*/}
                     <div className='flex gap-x-2 items-center cursor-pointer '>
-                        <div className=' flex rounded items-center justify-center bg-white w-6 h-6 '>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" PencilIcon color='#88b77b' viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
-                            </svg>
+                        <div className=' flex rounded items-center justify-center w-6 h-6 '>
+                        <BookmarkAddedOutlinedIcon sx={{ color: 'white', fontSize: 32}}/>  
                         </div>
                         <h1 className={`${!open&&'hidden'}  text-white font-bold tracking-wide`}>Saved</h1>
                     </div>  
