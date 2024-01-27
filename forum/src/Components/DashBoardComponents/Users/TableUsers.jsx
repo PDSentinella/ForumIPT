eac6811acb5a11c004a9c1085de4faac8cc4caa7
Cyb3rWolf945 <59710384+Cyb3rWolf945@users.noreply.github.com>
2024-01-27 18:36:11 +0000

    merge
    merfe

--- forum/src/Components/DashBoardComponents/Users/TableUsers.jsx
+++ forum/src/Components/DashBoardComponents/Users/TableUsers.jsx
@@ -1,6 +1,5 @@
 import React, { useMemo } from 'react'
 import { useEffect, useState } from 'react';
-<<<<<<< HEAD
 import { Avatar, Box, Button, Typography } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import {GetUsersDash} from '../../../services/DashBoard.api';
@@ -14,12 +13,6 @@ import DialogContent from '@mui/material/DialogContent';
 import SucessAlert from "../../SucessAlert";
 import FileBase64 from 'react-file-base64';
 import Grid from '@mui/material/Grid';
-=======
-import { Avatar, Box, Typography } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
-import {GetUsersDash} from '../../../services/DashBoard.api';
-import UserActions from './UserActions';
->>>>>>> parent of 8012fd7 (Revert "Merge branch 'antonio' into paulo")
 
 function TableUsers() {
 
@@ -29,21 +22,17 @@ function TableUsers() {
     //paginação disponivel através do pacote das tabelas.
     const [pageSize, setPageSize] = useState(5);
 
-<<<<<<< HEAD
     const [openDialog, setOpenDialog] = useState(false);
 
     const [alertOpen, setAlertOpen] = useState(false);
 
     const [ File , setFile] = useState("");
 
-=======
->>>>>>> parent of 8012fd7 (Revert "Merge branch 'antonio' into paulo")
     //para saber que linha esta ativa
     const [rowId, setRowId] = useState(null);
 
     useEffect(() => {
         async function getusers(){
-<<<<<<< HEAD
             let user = JSON.parse(localStorage.getItem('user'));
             const users = await GetUsersDash();
             
@@ -51,11 +40,6 @@ function TableUsers() {
             const filteredUsers = users.filter((userData) => userData.user_id !== user.user_id);
 
             setUsersDash(filteredUsers);
-=======
-            const users = await GetUsersDash();
-            console.log(users);
-            setUsersDash(users);
->>>>>>> parent of 8012fd7 (Revert "Merge branch 'antonio' into paulo")
         }
         getusers();
     }, []);
@@ -65,7 +49,6 @@ function TableUsers() {
             {field: 'profile_image', headerName: 'Avatar', width: 60, renderCell: params=> <Avatar src={params.row.profile_image}/>, sortable: false, filterable: false},
             {field: 'user_id', headerName: 'ID', width: 60},
             {field: 'nome', headerName: 'Nome', width: 170},
-<<<<<<< HEAD
             {field: 'email', headerName: 'Email', width: 200, editable: true},
             {field: 'password', headerName: 'Password', width: 170, sortable: false},
             {field: 'genero', headerName: 'Genero', width: 125, editable: true},
@@ -76,23 +59,11 @@ function TableUsers() {
             {field: 'locations', headerName: 'Localização', width: 170, editable: true},
             {field: 'Edit', headerName: 'Edit', type: 'actions', renderCell: (params) => (  <UserActions {...{ params, rowId, setRowId }} />)},
             {field: 'Remove', headerName: 'Remove', type: 'actions', renderCell: (params) => (  <UserActionsRemove {...{ params, rowId, setRowId }} />)},             
-=======
-            {field: 'email', headerName: 'Email', width: 200},
-            {field: 'password', headerName: 'Password', width: 170, sortable: false},
-            {field: 'genero', headerName: 'Genero', width: 150, editable: true},
-            {field: 'admin_privileges', headerName: 'Admin', width: 120, type:'boolean', editable: true},
-            {field: 'About me', headerName: 'About', width: 150},
-            {field: 'telefone', headerName: 'Telefone', width: 170},
-            {field: 'jobtitle', headerName: 'Profissão', width: 170},
-            {field: 'locations', headerName: 'Localização', width: 170},
-            {field: 'actions', headerName: 'Actions', type: 'actions', renderCell: (params) => (  <UserActions {...{ params, rowId, setRowId }} />)},        
->>>>>>> parent of 8012fd7 (Revert "Merge branch 'antonio' into paulo")
             ],
         [rowId] // pois se houver uma alteração na linha temos de renderizar os botoes
       );
 
 
-<<<<<<< HEAD
       // funções para tratamento do dialog e do respetivo submit
       function handleOpen(){
         setOpenDialog(!openDialog)
@@ -134,11 +105,6 @@ function TableUsers() {
     <Box sx={{display: 'flex', justifyContent: {xs:'flex-start', xl: 'flex-end'}, marginBottom: 1, marginRight: {xl: 1}, marginLeft: {xs: 1}}}>
         <Button onClick={() => {setOpenDialog(true)}} sx={{color: 'black'}}><GroupAddOutlinedIcon sx={{ fontSize: 30, cursor: 'pointer'}}/></Button>
     </Box>
-=======
-  return (
-   <Box sx={{ height: '80%' ,width:'100%'}}>
-    <Typography variant='h3' component='h3' sx={{ textAlign: 'center', mt: 3, mb: 3}}> Gestão de Utilizadores</Typography>
->>>>>>> parent of 8012fd7 (Revert "Merge branch 'antonio' into paulo")
     <DataGrid
         columns={columns}
         rows={usersDash}
@@ -155,7 +121,6 @@ function TableUsers() {
 
     </DataGrid>
    </Box>
-<<<<<<< HEAD
 
 
    <Dialog open={openDialog} onClose={handleClose}>
@@ -203,8 +168,6 @@ function TableUsers() {
       </Dialog>
     <SucessAlert open={alertOpen} handleClose={handleCloseAlert} />
     </>
-=======
->>>>>>> parent of 8012fd7 (Revert "Merge branch 'antonio' into paulo")
   )
 }
 
