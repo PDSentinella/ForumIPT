d4502e5a1b9b2be5172eede65c44423830f80068
Paulo <124280407+PDSentinella@users.noreply.github.com>
2024-01-29 18:16:28 +0000

    .

--- forum/src/Pages/HomePage.jsx
+++ forum/src/Pages/HomePage.jsx
@@ -81,6 +81,7 @@ function HomePage() {
           <div className='flex flex-col w-full '>
           <Header></Header>
           <div className='flex flex-col items-center '>
+          <input></input>
           <AddPost></AddPost>
           {publications === null?(<><div className='w-full h-48 '></div><CircularProgress color="success" /><div className='w-full h-96 flex'></div></>):
           (publications.length==0?<div className='flex w-96 h-96 py-48 justify-items-center ali'><h1 className='text-center'>Não esta escrito em nenhum canal<br/>se <strong onClick={handleOpenSideBar}>increva</strong> em uma canal</h1><button></button></div>:
