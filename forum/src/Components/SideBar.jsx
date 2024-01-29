497a4458ed058f9072bf8f6c85aa7bbe9924e0fb
Cyb3rWolf945 <59710384+Cyb3rWolf945@users.noreply.github.com>
2024-01-29 19:10:41 +0000

    update

--- forum/src/Components/SideBar.jsx
+++ forum/src/Components/SideBar.jsx
@@ -62,6 +62,9 @@ function SideBar(props){
         try{
             await RegisterChannel(formJson);
             setAlertOpen(true);
+            setTimeout(() => { 
+                window.location.reload();
+              }, 1000); // 1000 milliseconds = 1 second
         }catch(error){
 
         }
@@ -72,7 +75,6 @@ function SideBar(props){
        async function getChannels(){
         const channels = await GetAllChannels();
         setCanais(channels);
-        
        }
 
        getChannels();
